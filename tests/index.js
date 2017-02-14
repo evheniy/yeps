a18509359c10a24fb36a7e66d78af258f89c8bc6
Evheniy Bystrov <evheniy.bystrov@just-eat.com>
2017-02-14 10:16:32 +0000

    new tests for all and race

--- tests/index.js
+++ tests/index.js
@@ -1,3 +1,4 @@
+const pause = require('promise-pause-timeout');
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const http = require('http');
@@ -35,6 +36,52 @@ describe('YAPS test', () => {
             });
 
         return expect(isTestFinished).is.true;
+    });
+
+    it('should test 3 calls', async () => {
+
+        const text = 'okay';
+        const contentType = 'text/plain';
+        let isTestFinished1 = false;
+        let isTestFinished2 = false;
+        let isTestFinished3 = false;
+
+        app.then(async ctx => {
+            ctx.res.writeHead(200, {'Content-Type': contentType});
+            ctx.res.end(text);
+        });
+
+        await chai.request(http.createServer(app.resolve()))
+            .get('/')
+            .send()
+            .then(res => {
+                expect(res).to.have.status(200);
+                expect(res.text).to.be.equal(text);
+                expect(res.headers['content-type']).to.be.equal(contentType);
+                isTestFinished1 = true;
+            });
+        await chai.request(http.createServer(app.resolve()))
+            .get('/')
+            .send()
+            .then(res => {
+                expect(res).to.have.status(200);
+                expect(res.text).to.be.equal(text);
+                expect(res.headers['content-type']).to.be.equal(contentType);
+                isTestFinished2 = true;
+            });
+        await chai.request(http.createServer(app.resolve()))
+            .get('/')
+            .send()
+            .then(res => {
+                expect(res).to.have.status(200);
+                expect(res.text).to.be.equal(text);
+                expect(res.headers['content-type']).to.be.equal(contentType);
+                isTestFinished3 = true;
+            });
+
+        expect(isTestFinished1).is.true;
+        expect(isTestFinished2).is.true;
+        return expect(isTestFinished3).is.true;
     });
 
     it('should test chain of promises', async () => {
@@ -150,6 +197,93 @@ describe('YAPS test', () => {
             });
 
         expect(isTestOk).is.true;
+
+        return expect(isTestFinished).is.true;
+    });
+
+    it('should test all', async () => {
+
+        const text = 'okay';
+        const contentType = 'text/plain';
+        let isTestFinished = false;
+
+        app.all([
+            async ctx => {
+                ctx.headers = {};
+                ctx.headers['Content-Length'] = 4;
+            },
+            async ctx => {
+                ctx.headers['Content-Type'] = contentType;
+            }
+        ]);
+
+        app.then(async ctx => {
+            ctx.res.writeHead(200, ctx.headers);
+            ctx.res.end(text);
+        });
+
+        await chai.request(http.createServer(app.resolve()))
+            .get('/')
+            .send()
+            .then(res => {
+                expect(res).to.have.status(200);
+                expect(res.text).to.be.equal(text);
+                expect(res.headers['content-type']).to.be.equal(contentType);
+                expect(res.headers['content-length']).to.be.equal('4');
+                isTestFinished = true;
+            });
+
+        return expect(isTestFinished).is.true;
+    });
+
+    it('should test race', async () => {
+
+        let isTestFinished = false;
+
+        app.race([
+            async ctx => {
+                ctx.res.writeHead(200);
+                ctx.res.end('test');
+            },
+            async ctx => {
+                ctx.res.writeHead(200);
+                ctx.res.end('test');
+            }
+        ]);
+
+        await chai.request(http.createServer(app.resolve()))
+            .get('/')
+            .send()
+            .then(res => {
+                expect(res).to.have.status(200);
+                isTestFinished = true;
+            });
+
+        return expect(isTestFinished).is.true;
+    });
+
+    it('should test race without reject', async () => {
+
+        let isTestFinished = false;
+
+        app.race([
+            async () => {
+                await pause(10);
+                throw new Error('test');
+            },
+            async ctx => {
+                ctx.res.writeHead(200);
+                ctx.res.end('test');
+            }
+        ]);
+
+        await chai.request(http.createServer(app.resolve()))
+            .get('/')
+            .send()
+            .then(res => {
+                expect(res).to.have.status(200);
+                isTestFinished = true;
+            });
 
         return expect(isTestFinished).is.true;
     });
