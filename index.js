24351516d063327226c4f246a79a10d2fcb149b9
Evheniy Bystrov <evheniy.bystrov@gmail.com>
2017-07-10 20:35:36 +0300

    new eslint with airbnb config

--- index.js
+++ index.js
@@ -2,58 +2,72 @@ const promisify = require('yeps-promisify');
 const debug = require('debug')('yeps:server');
 
 module.exports = class {
-    constructor() {
-        debug('Server created');
-        this.promises = [];
-        this.error = [];
-    }
+  constructor() {
+    debug('Server created');
 
-    then(fn) {
-        debug('New promise added');
-        this.promises.push(fn);
-        return this;
-    }
+    this.promises = [];
+    this.error = [];
+  }
 
-    catch(fn) {
-        debug('Error handler added');
-        this.error.push(fn);
-        return this;
-    }
+  then(fn) {
+    debug('New promise added');
 
-    reject() {
-        debug('Reject');
-        return Promise.reject();
-    }
+    this.promises.push(fn);
 
-    resolve() {
-        debug('Server started');
-        return async (req, res) => {
-            const context = { req, res, app: this };
-            debug(context);
-            try {
-                await promisify(context, this.promises);
-                debug('End promisify');
-            } catch (error) {
-                debug('Error');
-                debug(error);
-                await Promise.all(this.error.map(fn => fn(error, context)));
-            }
-        };
-    }
+    return this;
+  }
 
-    all(fns) {
-        debug('all');
-        this.promises.push(async ctx => {
-            await Promise.all(fns.map(fn => fn(ctx)));
-        });
-        return this;
-    }
+  catch(fn) {
+    debug('Error handler added');
 
-    race(fns) {
-        debug('race');
-        this.promises.push(async ctx => {
-            await Promise.race(fns.map(fn => fn(ctx)));
-        });
-        return this;
-    }
+    this.error.push(fn);
+
+    return this;
+  }
+
+  reject() {
+    debug('Reject');
+
+    return Promise.reject();
+  }
+
+  resolve() {
+    debug('Server started');
+
+    return async (req, res) => {
+      const context = { req, res, app: this };
+
+      debug(context);
+
+      try {
+        await promisify(context, this.promises);
+        debug('End promisify');
+      } catch (error) {
+        debug('Error');
+        debug(error);
+
+        await Promise.all(this.error.map(fn => fn(error, context)));
+      }
+    };
+  }
+
+  all(fns) {
+    debug('all');
+
+    this.promises.push(async (ctx) => {
+      await Promise.all(fns.map(fn => fn(ctx)));
+    });
+
+    return this;
+  }
+
+  race(fns) {
+    debug('race');
+
+    this.promises.push(async (ctx) => {
+      await Promise.race(fns.map(fn => fn(ctx)));
+    });
+
+    return this;
+  }
 };
--- tests/index.js
+++ tests/index.js
@@ -2,361 +2,350 @@ const pause = require('promise-pause-timeout');
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const http = require('http');
-const expect = chai.expect;
 const App = require('../index');
+
+const expect = chai.expect;
 
 chai.use(chaiHttp);
 let app;
 
 describe('YAPS test', () => {
+  beforeEach(() => {
+    app = new App();
+  });
 
-    beforeEach(() => {
-        app = new App();
+  it('should test promise', async () => {
+    const text = 'okay';
+    const contentType = 'text/plain';
+
+    let isTestFinished = false;
+
+    app.then(async (ctx) => {
+      expect(ctx.app).to.be.equal(app);
+      ctx.res.writeHead(200, { 'Content-Type': contentType });
+      ctx.res.end(text);
     });
 
-    it('should test promise', async () => {
+    await chai.request(http.createServer(app.resolve()))
+      .get('/')
+      .send()
+      .then((res) => {
+        expect(res).to.have.status(200);
+        expect(res.text).to.be.equal(text);
+        expect(res.headers['content-type']).to.be.equal(contentType);
+        isTestFinished = true;
+      });
 
-        const text = 'okay';
-        const contentType = 'text/plain';
-        let isTestFinished = false;
+    expect(isTestFinished).is.true;
+  });
 
-        app.then(async ctx => {
-            expect(ctx.app).to.be.equal(app);
-            ctx.res.writeHead(200, {'Content-Type': contentType});
-            ctx.res.end(text);
-        });
+  it('should test 3 calls', async () => {
+    const text = 'okay';
+    const contentType = 'text/plain';
 
-        await chai.request(http.createServer(app.resolve()))
-            .get('/')
-            .send()
-            .then(res => {
-                expect(res).to.have.status(200);
-                expect(res.text).to.be.equal(text);
-                expect(res.headers['content-type']).to.be.equal(contentType);
-                isTestFinished = true;
-            });
+    let isTestFinished1 = false;
+    let isTestFinished2 = false;
+    let isTestFinished3 = false;
 
-        expect(isTestFinished).is.true;
+    app.then(async (ctx) => {
+      ctx.res.writeHead(200, { 'Content-Type': contentType });
+      ctx.res.end(text);
     });
 
-    it('should test 3 calls', async () => {
+    const server = http.createServer(app.resolve());
 
-        const text = 'okay';
-        const contentType = 'text/plain';
-        let isTestFinished1 = false;
-        let isTestFinished2 = false;
-        let isTestFinished3 = false;
+    await chai.request(server)
+      .get('/')
+      .send()
+      .then((res) => {
+        expect(res).to.have.status(200);
+        expect(res.text).to.be.equal(text);
+        expect(res.headers['content-type']).to.be.equal(contentType);
+        isTestFinished1 = true;
+      });
 
-        app.then(async ctx => {
-            ctx.res.writeHead(200, {'Content-Type': contentType});
-            ctx.res.end(text);
-        });
+    await chai.request(server)
+      .get('/')
+      .send()
+      .then((res) => {
+        expect(res).to.have.status(200);
+        expect(res.text).to.be.equal(text);
+        expect(res.headers['content-type']).to.be.equal(contentType);
+        isTestFinished2 = true;
+      });
 
-        const server = http.createServer(app.resolve());
+    await chai.request(server)
+      .get('/')
+      .send()
+      .then((res) => {
+        expect(res).to.have.status(200);
+        expect(res.text).to.be.equal(text);
+        expect(res.headers['content-type']).to.be.equal(contentType);
+        isTestFinished3 = true;
+      });
 
-        await chai.request(server)
-            .get('/')
-            .send()
-            .then(res => {
-                expect(res).to.have.status(200);
-                expect(res.text).to.be.equal(text);
-                expect(res.headers['content-type']).to.be.equal(contentType);
-                isTestFinished1 = true;
-            });
-        await chai.request(server)
-            .get('/')
-            .send()
-            .then(res => {
-                expect(res).to.have.status(200);
-                expect(res.text).to.be.equal(text);
-                expect(res.headers['content-type']).to.be.equal(contentType);
-                isTestFinished2 = true;
-            });
-        await chai.request(server)
-            .get('/')
-            .send()
-            .then(res => {
-                expect(res).to.have.status(200);
-                expect(res.text).to.be.equal(text);
-                expect(res.headers['content-type']).to.be.equal(contentType);
-                isTestFinished3 = true;
-            });
+    expect(isTestFinished1).is.true;
+    expect(isTestFinished2).is.true;
+    expect(isTestFinished3).is.true;
+  });
 
-        expect(isTestFinished1).is.true;
-        expect(isTestFinished2).is.true;
-        expect(isTestFinished3).is.true;
+  it('should test chain of promises', async () => {
+    const text = 'okay';
+    const contentType = 'text/plain';
+    let isTestFinished = false;
+
+    app.then(async (ctx) => {
+      ctx.headers = {};
+      ctx.headers['Content-Length'] = 4;
+    }).then(async (ctx) => {
+      ctx.headers['Content-Type'] = contentType;
     });
 
-    it('should test chain of promises', async () => {
-
-        const text = 'okay';
-        const contentType = 'text/plain';
-        let isTestFinished = false;
-
-        app.then(async ctx => {
-            ctx.headers = {};
-            ctx.headers['Content-Length'] = 4;
-        }).then(async ctx => {
-            ctx.headers['Content-Type'] = contentType;
-        });
-
-        app.then(async ctx => {
-            ctx.res.writeHead(200, ctx.headers);
-            ctx.res.end(text);
-        });
-
-        await chai.request(http.createServer(app.resolve()))
-            .get('/')
-            .send()
-            .then(res => {
-                expect(res).to.have.status(200);
-                expect(res.text).to.be.equal(text);
-                expect(res.headers['content-type']).to.be.equal(contentType);
-                expect(res.headers['content-length']).to.be.equal('4');
-                isTestFinished = true;
-            });
-
-        expect(isTestFinished).is.true;
+    app.then(async (ctx) => {
+      ctx.res.writeHead(200, ctx.headers);
+      ctx.res.end(text);
     });
 
-    it('should test error handler', async () => {
+    await chai.request(http.createServer(app.resolve()))
+      .get('/')
+      .send()
+      .then((res) => {
+        expect(res).to.have.status(200);
+        expect(res.text).to.be.equal(text);
+        expect(res.headers['content-type']).to.be.equal(contentType);
+        expect(res.headers['content-length']).to.be.equal('4');
+        isTestFinished = true;
+      });
 
-        const text = 'error';
-        let isTestFinished = false;
+    expect(isTestFinished).is.true;
+  });
 
-        app.then(async () => {
-            return Promise.reject(new Error(text));
-        });
+  it('should test error handler', async () => {
+    const text = 'error';
 
-        app.catch(async (err, ctx) => {
-            ctx.res.writeHead(200);
-            ctx.res.end(err.message);
-        });
+    let isTestFinished = false;
 
-        await chai.request(http.createServer(app.resolve()))
-            .get('/')
-            .send()
-            .then(res => {
-                expect(res).to.have.status(200);
-                expect(res.text).to.be.equal(text);
-                isTestFinished = true;
-            });
+    app.then(async () => Promise.reject(new Error(text)));
 
-        expect(isTestFinished).is.true;
+    app.catch(async (err, ctx) => {
+      ctx.res.writeHead(200);
+      ctx.res.end(err.message);
     });
 
-    it('should test error handler without Promise.reject()', async () => {
+    await chai.request(http.createServer(app.resolve()))
+      .get('/')
+      .send()
+      .then((res) => {
+        expect(res).to.have.status(200);
+        expect(res.text).to.be.equal(text);
+        isTestFinished = true;
+      });
 
-        const text = 'error';
-        let isTestFinished = false;
+    expect(isTestFinished).is.true;
+  });
 
-        app.then(async () => {
-            throw new Error(text);
-        });
+  it('should test error handler without Promise.reject()', async () => {
+    const text = 'error';
 
-        app.catch(async (err, ctx) => {
-            ctx.res.writeHead(200);
-            ctx.res.end(err.message);
-        });
+    let isTestFinished = false;
 
-        await chai.request(http.createServer(app.resolve()))
-            .get('/')
-            .send()
-            .then(res => {
-                expect(res).to.have.status(200);
-                expect(res.text).to.be.equal(text);
-                isTestFinished = true;
-            });
-
-        expect(isTestFinished).is.true;
+    app.then(async () => {
+      throw new Error(text);
     });
 
-    it('should test 2 error handlers', async () => {
-
-        const text = 'error';
-        let isTestFinished1 = false;
-        let isTestFinished2 = false;
-        let isTestFinished3 = false;
-
-        app.then(async () => {
-            return Promise.reject(new Error(text));
-        });
-
-        app.catch(async (err, ctx) => {
-            isTestFinished1 = true;
-
-            ctx.res.writeHead(200);
-            ctx.res.end(err.message);
-
-        }).catch(async () => {
-
-            isTestFinished2 = true;
-
-        });
-
-        await chai.request(http.createServer(app.resolve()))
-            .get('/')
-            .send()
-            .then(res => {
-                expect(res).to.have.status(200);
-                expect(res.text).to.be.equal(text);
-                isTestFinished3 = true;
-            });
-
-        expect(isTestFinished1).is.true;
-        expect(isTestFinished2).is.true;
-        expect(isTestFinished3).is.true;
+    app.catch(async (err, ctx) => {
+      ctx.res.writeHead(200);
+      ctx.res.end(err.message);
     });
 
-    it('should test breaking of promises chain without catch', async () => {
+    await chai.request(http.createServer(app.resolve()))
+      .get('/')
+      .send()
+      .then((res) => {
+        expect(res).to.have.status(200);
+        expect(res.text).to.be.equal(text);
+        isTestFinished = true;
+      });
 
-        let isTestFinished = false;
-        let isTestOk = false;
+    expect(isTestFinished).is.true;
+  });
 
-        app.then(async ctx => {
-            ctx.res.writeHead(200);
-            ctx.res.end('test');
+  it('should test 2 error handlers', async () => {
+    const text = 'error';
 
-            return app.reject();
-        });
+    let isTestFinished1 = false;
+    let isTestFinished2 = false;
+    let isTestFinished3 = false;
 
-        app.then(async () => {
-            isTestOk = false;
-        });
+    app.then(async () => Promise.reject(new Error(text)));
 
-        app.catch(async () => {
-            isTestOk = true;
-        });
+    app.catch(async (err, ctx) => {
+      isTestFinished1 = true;
 
-        await chai.request(http.createServer(app.resolve()))
-            .get('/')
-            .send()
-            .then(res => {
-                expect(res).to.have.status(200);
-                isTestFinished = true;
-            });
-
-        expect(isTestOk).is.true;
-        expect(isTestFinished).is.true;
+      ctx.res.writeHead(200);
+      ctx.res.end(err.message);
+    }).catch(async () => {
+      isTestFinished2 = true;
     });
 
-    it('should test breaking of promises chain with catch', async () => {
+    await chai.request(http.createServer(app.resolve()))
+      .get('/')
+      .send()
+      .then((res) => {
+        expect(res).to.have.status(200);
+        expect(res.text).to.be.equal(text);
+        isTestFinished3 = true;
+      });
 
-        let isTestFinished = false;
-        let isTestOk = false;
+    expect(isTestFinished1).is.true;
+    expect(isTestFinished2).is.true;
+    expect(isTestFinished3).is.true;
+  });
 
-        app.then(async ctx => {
-            ctx.res.writeHead(200);
-            ctx.res.end('test');
+  it('should test breaking of promises chain without catch', async () => {
+    let isTestFinished = false;
+    let isTestOk = false;
 
-            return app.reject(123);
-        });
+    app.then(async (ctx) => {
+      ctx.res.writeHead(200);
+      ctx.res.end('test');
 
-        app.then(async () => {
-            isTestOk = false;
-        });
-
-        app.catch(async () => {
-            isTestOk = true;
-        });
-
-        await chai.request(http.createServer(app.resolve()))
-            .get('/')
-            .send()
-            .then(res => {
-                expect(res).to.have.status(200);
-                isTestFinished = true;
-            });
-
-        expect(isTestOk).is.true;
-        expect(isTestFinished).is.true;
+      return app.reject();
     });
 
-    it('should test all', async () => {
-
-        const text = 'okay';
-        const contentType = 'text/plain';
-        let isTestFinished = false;
-
-        app.all([
-            async ctx => {
-                ctx.headers = {};
-                ctx.headers['Content-Length'] = 4;
-            },
-            async ctx => {
-                ctx.headers['Content-Type'] = contentType;
-            }
-        ]);
-
-        app.then(async ctx => {
-            ctx.res.writeHead(200, ctx.headers);
-            ctx.res.end(text);
-        });
-
-        await chai.request(http.createServer(app.resolve()))
-            .get('/')
-            .send()
-            .then(res => {
-                expect(res).to.have.status(200);
-                expect(res.text).to.be.equal(text);
-                expect(res.headers['content-type']).to.be.equal(contentType);
-                expect(res.headers['content-length']).to.be.equal('4');
-                isTestFinished = true;
-            });
-
-        expect(isTestFinished).is.true;
+    app.then(async () => {
+      isTestOk = false;
     });
 
-    it('should test race', async () => {
-
-        let isTestFinished = false;
-
-        app.race([
-            async ctx => {
-                ctx.res.writeHead(200);
-                ctx.res.end('test');
-            },
-            async ctx => {
-                ctx.res.writeHead(200);
-                ctx.res.end('test');
-            }
-        ]);
-
-        await chai.request(http.createServer(app.resolve()))
-            .get('/')
-            .send()
-            .then(res => {
-                expect(res).to.have.status(200);
-                isTestFinished = true;
-            });
-
-        expect(isTestFinished).is.true;
+    app.catch(async () => {
+      isTestOk = true;
     });
 
-    it('should test race without reject', async () => {
+    await chai.request(http.createServer(app.resolve()))
+      .get('/')
+      .send()
+      .then((res) => {
+        expect(res).to.have.status(200);
+        isTestFinished = true;
+      });
 
-        let isTestFinished = false;
+    expect(isTestOk).is.true;
+    expect(isTestFinished).is.true;
+  });
 
-        app.race([
-            async () => {
-                await pause(10);
-                throw new Error('test');
-            },
-            async ctx => {
-                ctx.res.writeHead(200);
-                ctx.res.end('test');
-            }
-        ]);
+  it('should test breaking of promises chain with catch', async () => {
+    let isTestFinished = false;
+    let isTestOk = false;
 
-        await chai.request(http.createServer(app.resolve()))
-            .get('/')
-            .send()
-            .then(res => {
-                expect(res).to.have.status(200);
-                isTestFinished = true;
-            });
+    app.then(async (ctx) => {
+      ctx.res.writeHead(200);
+      ctx.res.end('test');
 
-        expect(isTestFinished).is.true;
+      return app.reject(123);
     });
 
+    app.then(async () => {
+      isTestOk = false;
+    });
+
+    app.catch(async () => {
+      isTestOk = true;
+    });
+
+    await chai.request(http.createServer(app.resolve()))
+      .get('/')
+      .send()
+      .then((res) => {
+        expect(res).to.have.status(200);
+        isTestFinished = true;
+      });
+
+    expect(isTestOk).is.true;
+    expect(isTestFinished).is.true;
+  });
+
+  it('should test all', async () => {
+    const text = 'okay';
+    const contentType = 'text/plain';
+
+    let isTestFinished = false;
+
+    app.all([
+      async (ctx) => {
+        ctx.headers = {};
+        ctx.headers['Content-Length'] = 4;
+      },
+      async (ctx) => {
+        ctx.headers['Content-Type'] = contentType;
+      },
+    ]);
+
+    app.then(async (ctx) => {
+      ctx.res.writeHead(200, ctx.headers);
+      ctx.res.end(text);
+    });
+
+    await chai.request(http.createServer(app.resolve()))
+      .get('/')
+      .send()
+      .then((res) => {
+        expect(res).to.have.status(200);
+        expect(res.text).to.be.equal(text);
+        expect(res.headers['content-type']).to.be.equal(contentType);
+        expect(res.headers['content-length']).to.be.equal('4');
+        isTestFinished = true;
+      });
+
+    expect(isTestFinished).is.true;
+  });
+
+  it('should test race', async () => {
+    let isTestFinished = false;
+
+    app.race([
+      async (ctx) => {
+        ctx.res.writeHead(200);
+        ctx.res.end('test');
+      },
+      async (ctx) => {
+        ctx.res.writeHead(200);
+        ctx.res.end('test');
+      },
+    ]);
+
+    await chai.request(http.createServer(app.resolve()))
+      .get('/')
+      .send()
+      .then((res) => {
+        expect(res).to.have.status(200);
+        isTestFinished = true;
+      });
+
+    expect(isTestFinished).is.true;
+  });
+
+  it('should test race without reject', async () => {
+    let isTestFinished = false;
+
+    app.race([
+      async () => {
+        await pause(10);
+        throw new Error('test');
+      },
+      async (ctx) => {
+        ctx.res.writeHead(200);
+        ctx.res.end('test');
+      },
+    ]);
+
+    await chai.request(http.createServer(app.resolve()))
+      .get('/')
+      .send()
+      .then((res) => {
+        expect(res).to.have.status(200);
+        isTestFinished = true;
+      });
+
+    expect(isTestFinished).is.true;
+  });
 });
